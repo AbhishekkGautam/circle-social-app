fa30bfaa57a0808c2db66b780398c411b77709b5
AbhishekkGautam <46835940+AbhishekkGautam@users.noreply.github.com>
2022-05-18 14:21:19 +0530

    feat: added private routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Mockman from "mockman-js";
+import { useSelector } from "react-redux";
 import "./App.css";
 import { Explore, Home, Login, Profile, Signup, SinglePost } from "./pages";
 import { ToasterWrapper } from "./utils";
+import { PrivateRoute } from "./routes/PrivateRoutes";
 
 function App() {
+  const { isLoggedIn } = useSelector(state => state.auth);
   return (
     <div>
       <ToasterWrapper />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:postId" element={<SinglePost />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:postId" element={<SinglePost />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/explore" element={<Explore />} />
+        </Route>
+        {!isLoggedIn && <Route path="/signup" element={<Signup />} />}
+        {!isLoggedIn && <Route path="/login" element={<Login />} />}
         <Route path="/mock" element={<Mockman />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
