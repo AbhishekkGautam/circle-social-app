7d0cfdca71af048bd34b81f527ce5fe20b673d06
AbhishekkGautam <46835940+AbhishekkGautam@users.noreply.github.com>
2022-05-21 15:30:27 +0530

    refactor bottom nav

--- src/components/BottomNavigation/BottomNavigation.jsx
+++ src/components/BottomNavigation/BottomNavigation.jsx
@@ -4,16 +4,42 @@ import {
   BookmarkIcon,
   HomeIcon,
   UserIcon,
+  LogoutIcon,
 } from "@heroicons/react/outline";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut } from "../../features/auth/authSlice";
 
 export const BottomNavigation = () => {
+  const { userInfo } = useSelector(state => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logOutHandler = () => {
+    dispatch(logOut());
+    navigate("/login");
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+
   return (
     <div className="sm:hidden fixed bottom-0 bg-[#151F2B] border-t border-gray-700 w-full z-50 px-6">
       <div className="flex items-center justify-between py-5">
-        <HomeIcon className="h-6 text-[#d9d9d9]" />
-        <HashtagIcon className="h-6 text-[#d9d9d9]" />
-        <BookmarkIcon className="h-6 text-[#d9d9d9]" />
-        <UserIcon className="h-6 text-[#d9d9d9]" />
+        <Link to="/">
+          <HomeIcon className="h-6 text-[#d9d9d9]" />
+        </Link>
+        <Link to="/explore">
+          <HashtagIcon className="h-6 text-[#d9d9d9]" />
+        </Link>
+        <Link to="/bookmarks">
+          <BookmarkIcon className="h-6 text-[#d9d9d9]" />
+        </Link>
+        <Link to={`/profile/${userInfo?.username}`}>
+          <UserIcon className="h-6 text-[#d9d9d9]" />
+        </Link>
+
+        <LogoutIcon className="h-6 text-[#d9d9d9]" onClick={logOutHandler} />
       </div>
     </div>
   );
